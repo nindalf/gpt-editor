55fdd12c0ea3464b51fe7ed1f6a9d0ec8ba43af2
Krishna Sundarram <krishnasundarram@monzo.com>
2023-03-19 08:40:58 +0000

    Add shortSummary command

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,14 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+
 import axios from 'axios';
+import { Configuration, OpenAIApi, CreateChatCompletionRequest, ChatCompletionRequestMessage } from "openai";
+
+const configuration = new Configuration({
+	apiKey: 'todo',
+  });
+  
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -11,23 +18,50 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "markdown-gpt" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('markdown-gpt.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello from Markdown-GPT!');
-	});	
-	
 	let ping = vscode.commands.registerCommand('markdown-gpt.ping', async () => {
 		let response = await axios.get("https://nindalf.com/kudos?url=https://nindalf.com/");
 		const {kudo_count} = response.data;
 		
 		vscode.window.showInformationMessage(`Current kudo count is ${kudo_count}`);
 	});
+	
+	const openai = new OpenAIApi(configuration);
+	let shortSummary = vscode.commands.registerCommand('markdown-gpt.shortSummary', async () => {
+		let input_text = getText();
+		console.log(input_text);
+		let messages: Array<ChatCompletionRequestMessage> = [
+			{"role": "system", "content": "You are a helpful assistant."},
+			{"role": "user", "content": `Consider this article:\n${input_text}`},
+			{"role": "user", "content": "Please write a one sentence blurb of this article"},
+		];
+		let request: CreateChatCompletionRequest = {
+			model: 'gpt-3.5-turbo',
+			max_tokens: 60,
+			n: 1,
+			temperature: 0.5,
+			messages: messages
+		}
+		let resp = await openai.createChatCompletion(request);
+		let { choices } = resp.data;
+		let [first_choice] = choices;
+		let summary = first_choice.message?.content;
+		vscode.window.showInformationMessage(`Suggested summary:${summary}`);
+	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(ping, shortSummary);
+}
+
+function getText() {
+	const editor = vscode.window.activeTextEditor;
+
+	if (!editor) {
+	  vscode.window.showErrorMessage("No active editor");
+	  return;
+	}
+  
+	const selection = editor.selection;
+	return editor.document.getText(selection);
+  
 }
 
 // This method is called when your extension is deactivated
