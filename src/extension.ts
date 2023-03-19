3fa8dc58a1599f1ecd01927b6e7a89fd9e6aa176
Krishna Sundarram <krishnasundarram@monzo.com>
2023-03-19 13:15:55 +0000

    Add second command

--- src/extension.ts
+++ src/extension.ts
@@ -9,65 +9,83 @@ import { Configuration, OpenAIApi, CreateChatCompletionRequest, ChatCompletionRe
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "gpt-editor" is now active!');
 
-	let ping = vscode.commands.registerCommand('gpt-editor.ping', async () => {
-		let response = await axios.get("https://nindalf.com/kudos?url=https://nindalf.com/");
+	const ping = vscode.commands.registerCommand('gpt-editor.ping', async () => {
+		const response = await axios.get("https://nindalf.com/kudos?url=https://nindalf.com/");
 		const { kudo_count } = response.data;
 
 		vscode.window.showInformationMessage(`Current kudo count is ${kudo_count}`);
 	});
 
-	let shortSummary = vscode.commands.registerCommand('gpt-editor.shortSummary', async () => {
-		let settings = vscode.workspace.getConfiguration()
-		let api_key = settings.get('gpt-editor.openai_api_key')
-		if (!api_key) {
-			vscode.window.showErrorMessage('Please set the OpenAI API Key');
-			return;
-		}
-		const configuration = new Configuration({
-			apiKey: settings.get('gpt-editor.openai_api_key'),
-		});
-		const openai = new OpenAIApi(configuration);
-		let input_text = getText();
-		console.log(input_text);
-		let messages: Array<ChatCompletionRequestMessage> = [
-			{ "role": "system", "content": "You are a helpful assistant." },
-			{ "role": "user", "content": `Consider this article:\n${input_text}` },
-			{ "role": "user", "content": "Please write a one sentence blurb of this article" },
-		];
-		let request: CreateChatCompletionRequest = {
-			model: settings.get('gpt-editor.model', 'gpt-3.5-turbo'),
-			max_tokens: 60,
-			n: 1,
-			temperature: settings.get('gpt-editor.temperature', 0.5),
-			top_p: settings.get('gpt-editor.top_p', 0.5),
-			messages: messages
-		}
-		let resp = await openai.createChatCompletion(request);
-		let { choices } = resp.data;
-		let [first_choice] = choices;
-		let summary = first_choice.message?.content;
-		vscode.window.showInformationMessage(`Suggested summary:${summary}`);
-	});
+	const commandPrompts = [
+		['gpt-editor.shortBlurb', `Please write a one sentence blurb of this text. Don't add any additional text before or after.`],
+		['gpt-editor.summary', `Please write a one paragraph summary of this text. Don't add any additional text before or after.`],
+	]
 
-	context.subscriptions.push(ping, shortSummary);
+	for (const [command, prompt] of commandPrompts) {
+		const item = vscode.commands.registerCommand(command, async () => {
+			const input_text = getText();
+			console.log(input_text);
+			const summary = await callOpenAIAPI(input_text, prompt);
+			vscode.window.showInformationMessage(`Suggested summary:${summary}`);
+		});
+		context.subscriptions.push(item);
+	}
 }
 
-function getText() {
+function getText(): string {
 	const editor = vscode.window.activeTextEditor;
 
 	if (!editor) {
 		vscode.window.showErrorMessage("No active editor");
-		return;
+		return '';
 	}
 
 	const selection = editor.selection;
-	return editor.document.getText(selection);
+	if (!selection.isEmpty) {
+		return editor.document.getText(selection);
+	}
 
+	const entireRange = new vscode.Range(
+		0,
+		0,
+		editor.document.lineCount - 1,
+		editor.document.lineAt(editor.document.lineCount - 1).text.length
+	  );
+	  return editor.document.getText(entireRange);
+}
+
+async function callOpenAIAPI(input_text: string, prompt: string) {
+	const settings = vscode.workspace.getConfiguration()
+	const api_key = settings.get('gpt-editor.openai_api_key')
+	if (!api_key) {
+		vscode.window.showErrorMessage('Please set the OpenAI API Key');
+		return;
+	}
+	const configuration = new Configuration({
+		apiKey: settings.get('gpt-editor.openai_api_key'),
+	});
+	const openai = new OpenAIApi(configuration);
+	const messages: Array<ChatCompletionRequestMessage> = [
+		{ "role": "system", "content": "You are a helpful assistant." },
+		{ "role": "user", "content": `Consider this article:\n${input_text}` },
+		{ "role": "user", "content": prompt },
+	];
+	const request: CreateChatCompletionRequest = {
+		model: settings.get('gpt-editor.model', 'gpt-3.5-turbo'),
+		max_tokens: 60,
+		n: 1,
+		temperature: settings.get('gpt-editor.temperature', 0.5),
+		top_p: settings.get('gpt-editor.top_p', 0.5),
+		messages: messages
+	}
+	const resp = await openai.createChatCompletion(request);
+	
+	const { choices } = resp.data;
+	const [first_choice] = choices;
+	const result = first_choice.message?.content;
+	return result;
 }
 
 // This method is called when your extension is deactivated
